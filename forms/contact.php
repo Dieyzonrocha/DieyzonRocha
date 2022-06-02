25193e6559bbbc79f2c566ea6c72e0e5108766de
Sr. D <79281349+Dieyzonrocha@users.noreply.github.com>
2022-06-02 10:20:22 -0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -38,5 +38,5 @@
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  echo $contact->send(dieyzon.rocha@gmail.com);
 ?>
